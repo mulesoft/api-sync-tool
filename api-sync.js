6fc5866234f1c8fb2ad5a8c21c99595e8cf4679a
Juan Coen <juan.coen@mulesoft.com>
2015-05-28 18:13:44 -0300

    Refactor: make CLI minimal by extracting code to a module

---
+++ api-sync.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var _ = require('lodash');
+var omelette = require('omelette');
+
+module.exports = function (args) {
+  var commandName = args[0];
+
+  // Get available commands.
+  var commands = fs.readdirSync(path.join(path.resolve(__dirname), 'commands'));
+  // Remove .js from command file names.
+  commands = _.map(commands, function (command) {
+    return command.split('.')[0];
+  });
+
+  // Setup autocomplete
+  var complete = omelette("api-sync <command>");
+
+  complete.on("command", function() {
+    this.reply(commands);
+  });
+
+  // Initialize the omelette.
+  complete.init();
+
+  if (args < 1) {
+    console.log('Error: Missing command name.');
+    console.log('Usage: api-sync <' + commands.join('|') + '>');
+    process.exit(0);
+  }
+
+  if (!_.includes(commands, commandName)) {
+    console.log('Unknown command: ' + commandName);
+    console.log('Usage: api-sync <' + commands.join('|') + '>');
+    process.exit(0);
+  }
+
+  var command = require('./commands/' + commandName);
+  command.execute(args)
+    .then(console.log)
+    .catch(console.log);
+};
