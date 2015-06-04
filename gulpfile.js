6c1e83247a19d76dcbdc0bc4547392a2be0ee3b5
Juan Coen <juan.coen@mulesoft.com>
2015-06-04 18:08:21 -0300

    Added test boilerplate and csRepository.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,47 @@
 'use strict';
 
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
+
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 
+var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
+
 gulp.task('lint', function () {
-  return gulp.src(['./**/*.js', '!./node_modules/**'])
+  return gulp.src(['./**/*.js', '!./node_modules/**', '!./coverage/**'])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('jscs', function () {
-  return gulp.src(['./**/*.js', '!./node_modules/**'])
+  return gulp.src(['./**/*.js', '!./node_modules/**', '!./coverage/**'])
     .pipe(jscs({
       configPath: '.jscs.json'
     }));
 });
 
-gulp.task('default', ['lint', 'jscs']);
+// To run test within a particular test file, run "gulp test --file theTestFile.js"
+// Test file must be under ./tests/ directory and this prefix should be ommited.
+// Example: To execute test for tests/unit/csRepository.test.js run "gulp test --file unit/csRepository.test.js"
+gulp.task('mocha', function (done) {
+  process.env.NODE_ENV = 'test';
+
+  gulp.src(['./commands/*.js', './controllers/*.js', './factories/*.js', './repositories/*.js', './services/*.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', function () {
+      var parameterIndex = process.argv.indexOf('--file');
+      var testFiles = (parameterIndex !== -1) ? './tests/' + process.argv[parameterIndex + 1] : './tests/unit/*.test.js';
+      gulp.src(['./tests/support/*.js', testFiles], {read: false})
+        .pipe(mocha({reporter: 'spec'}))
+        .pipe(istanbul.writeReports())
+        .on('end', done);
+    });
+});
+
+gulp.task('default', function (done) {
+  runSequence('lint', 'jscs', 'mocha', done);
+});
---
+++ tests/unit/csRepository.test.js
@@ -0,0 +1,74 @@
+'use strict';
+
+require('should');
+var sinon = require('sinon');
+
+var containerFactory  = require('../support/testContainerFactory');
+
+var superagentStub = {};
+
+describe('csRepository', function () {
+  beforeEach(function () {
+    superagentStub.get = sinon.stub().returnsThis();
+    superagentStub.post = sinon.stub().returnsThis();
+    superagentStub.send = sinon.stub().returnsThis();
+    superagentStub.set = sinon.stub().returnsThis();
+    superagentStub.end = sinon.stub().returns(Promise.resolve({
+      body: {}
+    }));
+  });
+
+  describe('login', run(function (csRepository) {
+    it('should login user', function (done) {
+      csRepository.login('juan', 'test')
+        .then(function () {
+          superagentStub.post.called.should.be.true;
+          superagentStub.post.firstCall.args[0].should.match(/.*\/oauth2\/token$/);
+
+          superagentStub.send.called.should.be.true;
+          superagentStub.send.firstCall.args[0].username.should.match('juan');
+          superagentStub.send.firstCall.args[0].password.should.match('test');
+
+          superagentStub.set.called.should.be.true;
+          superagentStub.set.firstCall.args[0].should.match('Accept');
+          superagentStub.set.firstCall.args[1].should.match('application/json');
+
+          superagentStub.end.called.should.be.true;
+
+          done();
+        })
+        .catch(function (err) {
+          done(err);
+        });
+    });
+  }));
+
+  describe('userInfo', run(function (csRepository) {
+    it('should get userInfo', function (done) {
+      csRepository.getUserInfo()
+        .then(function () {
+          superagentStub.get.called.should.be.true;
+          superagentStub.get.firstCall.args[0].should.match(/.*\/api\/users\/me$/);
+
+          superagentStub.set.called.should.be.true;
+          superagentStub.set.firstCall.args[0].should.match('Authorization');
+          superagentStub.set.firstCall.args[1].should.match(/Bearer.*/);
+
+          superagentStub.end.called.should.be.true;
+
+          done();
+        })
+        .catch(function (err) {
+          done(err);
+        });
+    });
+  }));
+});
+
+function run(callback) {
+  return function () {
+    var container = containerFactory.createContainer();
+    container.register('superagent', superagentStub);
+    container.resolve(callback);
+  };
+}
