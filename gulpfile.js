6ce07b20c137d16dc287c1fc88939e6bda805ec3
Juan Coen <juan.coen@mulesoft.com>
2015-07-02 11:34:43 -0300

    Integration tests first approach
    - Refactor test tasks in Gulp file.
    - Added first basic integration test

--- gulpfile.js
+++ gulpfile.js
@@ -23,27 +23,49 @@ gulp.task('jscs', function () {
     }));
 });
 
-// To run test within a particular test file, run "gulp test --file theTestFile.js"
-// Test file must be under ./tests/ directory and this prefix should be ommited.
-// Example: To execute test for tests/unit/csRepository.test.js run "gulp test --file unit/csRepository.test.js"
-gulp.task('mocha', function (done) {
+// To run test within a particular test file, run "gulp unit --file theTestFile.js"
+// Test file must be under ./tests/unit/ directory and this prefix should be ommited.
+// Example: To execute test for tests/unit/csRepository.test.js run "gulp unit --file csRepository.test.js"
+gulp.task('unit', function (done) {
   process.env.NODE_ENV = 'test';
 
+  var parameterIndex = process.argv.indexOf('--file');
+  var testFiles = (parameterIndex !== -1) ?
+    './tests/unit/' + process.argv[parameterIndex + 1] :
+    './tests/unit/*.test.js';
+
+  testWithCoverage(testFiles, done);
+});
+
+// To run test within a particular test file, run "gulp integration --file theTestFile.js"
+// Test file must be under ./tests/integration/ directory and this prefix should be ommited.
+// Example: To execute test for tests/integration/setup.test.js run "gulp integration --file setup.test.js"
+gulp.task('integration', function (done) {
+  var parameterIndex = process.argv.indexOf('--file');
+  var testFiles = (parameterIndex !== -1) ?
+    './tests/integration/' + process.argv[parameterIndex + 1] :
+    './tests/integration/*.test.js';
+
+  runTests(testFiles)
+    .on('end', done);
+});
+
+function testWithCoverage(testFiles, done) {
   gulp.src(['./lib/**/*.js'])
     .pipe(istanbul())
     .pipe(istanbul.hookRequire())
     .on('finish', function () {
-      var parameterIndex = process.argv.indexOf('--file');
-      var testFiles = (parameterIndex !== -1) ?
-        './tests/' + process.argv[parameterIndex + 1] :
-        './tests/unit/*.test.js';
-      gulp.src(['./tests/support/*.js', testFiles], {read: false})
-        .pipe(mocha({reporter: 'spec'}))
+      runTests(testFiles)
         .pipe(istanbul.writeReports())
         .on('end', done);
     });
-});
+}
+
+function runTests(testFiles) {
+  return gulp.src(['./tests/support/*.js', testFiles], {read: false})
+    .pipe(mocha({reporter: 'spec'}));
+}
 
 gulp.task('default', function (done) {
-  runSequence('lint', 'jscs', 'mocha', done);
+  runSequence('lint', 'jscs', 'unit', 'integration', done);
 });
