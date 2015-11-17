50515f58f104394eb8f7ced52eb93a3ef1b038e2
Juan Coen <juan.coen@mulesoft.com>
2015-11-17 19:31:45 -0300

    Add support to use api-sync as a library (first steps)
    - Add main file for library and configured it in package.json
    - Add some methods needed by the library

--- tests/unit/decompresser.test.js
+++ tests/unit/decompresser.test.js
@@ -6,15 +6,19 @@ var sinon = require('sinon');
 var asserts = require('../support/asserts');
 var containerFactory  = require('../support/testContainerFactory');
 
+var contextHolderStub = {};
+var contextStub = {};
 var errorsStub = {};
 var fsStub = {};
 var unzipStub = {};
+
+var path = '/dir/';
 
 describe('decompresser', function () {
   var compressedFilePath = 'pepe.zip';
   var expectedError = {error: 'pepe'};
   var unzipParameters = {
-    path: '.'
+    path: path
   };
   var unzipResult = {};
 
@@ -22,6 +26,8 @@ describe('decompresser', function () {
     fsStub.createReadStream = sinon.stub().returnsThis();
     fsStub.pipe = sinon.stub().returnsThis();
     fsStub.on = sinon.stub();
+    contextStub.getDirectoryPath = sinon.stub().returns(path);
+    contextHolderStub.get = sinon.stub().returns(contextStub);
 
     unzipStub.Extract = sinon.stub().returns(unzipResult);
   });
@@ -138,6 +144,7 @@ describe('decompresser', function () {
 
 function run(callback) {
   var container = containerFactory.createContainer();
+  container.register('contextHolder', contextHolderStub);
   container.register('errors', errorsStub);
   container.register('fs', fsStub);
   container.register('unzip', unzipStub);
